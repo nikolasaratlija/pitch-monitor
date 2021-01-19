ed9b6be1720b2b2fb3a59ca7ba47d2783f16602a
Nikola Saratlija <saratlijanikola@gmail.com>
2021-01-19 16:42:51 +0100

    comments

--- pitch_detection.py
+++ pitch_detection.py
@@ -2,12 +2,13 @@ import pyaudio
 import numpy as np
 from aubio import pitch
 
-
+# Sets up PyAudio to start recording
 p = pyaudio.PyAudio()
 BUFFER_SIZE = 1024
 SAMPLE_RATE = 44100
 stream = p.open(format=pyaudio.paFloat32, channels=1, rate=SAMPLE_RATE, input=True, frames_per_buffer=BUFFER_SIZE)
 
+# Sets up Aubio.pitch function which calculates the pitch from an audio stream
 HOP_SIZE = 4096
 pitch_o = pitch("yin", HOP_SIZE, BUFFER_SIZE, SAMPLE_RATE)
 pitch_o.set_unit("Hz")
@@ -16,13 +17,16 @@ pitch_o.set_tolerance(.8)
 all_pitches = []
 
 while True:
+    # Gets raw audio data
     data = stream.read(BUFFER_SIZE)
     sample = np.frombuffer(data, dtype=np.float32)
 
-    confidence = pitch_o.get_confidence()
+    # Gets the pitch and pitch confidence from the raw audio data
     pitch = pitch_o(sample)[0]
+    confidence = pitch_o.get_confidence()
 
     if confidence > 0.85 and (150 < pitch < 700):
+        # Pitches get appended to a list to then calculate the average pitch
         all_pitches.append(pitch)
-        avg_pitch = round(sum(all_pitches) / len(all_pitches), 2)
-        print('\r'+str(avg_pitch), end='')
+        average_pitch = round(sum(all_pitches) / len(all_pitches), 2)
+        print('\r' + str(average_pitch), end='')
